fd083ce3205f8e0c1b32f690f4e9734b1c70d8eb
XxdalendxX <connordr112@gmail.com>
2022-06-17 16:47:48 +1000

    (binaryTree) fixed resolution

--- CodeDesignAndDataStructures/BinaryTree/RaylibStarterCPP/ProjectMain.cpp
+++ CodeDesignAndDataStructures/BinaryTree/RaylibStarterCPP/ProjectMain.cpp
@@ -31,8 +31,8 @@ int main(int argc, char* argv[])
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    int screenWidth = 420;
-    int screenHeight = 200;
+    int screenWidth = 1800;
+    int screenHeight = 900;
 
     InitWindow(screenWidth, screenHeight, "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let's shake it up a little. Barry! Breakfast is ready! Coming! Hang on a second. Hello? -barry? -Adam? -Can you believe this is happening? -I can't. I'll pick you up. Looking sharp...");
 
