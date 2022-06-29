345dfadb808131dcf4cf21b5329cd49d0430cae8
XxdalendxX <connordr112@gmail.com>
2022-06-29 16:33:41 +1000

    Hashing Project added to work on

---
+++ CodeDesignAndDataStructures/HashingProject/RaylibStarterCPP/Hashing.cpp
@@ -0,0 +1,14 @@
+#include "Hashing.h"
+
+namespace HashFunction {
+
+    // implementation of a basic addition hash
+    unsigned int badHash(const char* data, unsigned int length) {
+        unsigned int hash = 0;
+
+        for (unsigned int i = 0; i < length; ++i)
+            hash += data[i];
+
+        return hash;
+    }
+}
---
+++ CodeDesignAndDataStructures/HashingProject/RaylibStarterCPP/Hashing.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <functional>
+
+namespace HashFunction {
+
+    typedef std::function< unsigned int(const char*, unsigned int)> HashFunc;
+
+    // implementation of a basic addition hash
+    unsigned int badHash(const char* data, unsigned int length);
+
+    // ADD YOUR FUNCTIONS HERE
+
+    // a helper to access a default hash function
+    static HashFunc default = badHash;
+}
