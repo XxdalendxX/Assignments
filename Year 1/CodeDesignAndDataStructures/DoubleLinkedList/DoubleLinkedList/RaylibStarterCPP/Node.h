78c94fe16fc69a3852c803ce070bab2386fdaa99
XxdalendxX <connordr112@gmail.com>
2022-08-24 16:40:21 +1000

    previous apply

--- Year 1/CodeDesignAndDataStructures/DoubleLinkedList/DoubleLinkedList/RaylibStarterCPP/Node.h
+++ Year 1/CodeDesignAndDataStructures/DoubleLinkedList/DoubleLinkedList/RaylibStarterCPP/Node.h
@@ -7,41 +7,10 @@ public:
 	Node();
 	Node(int value, Node* next, Node* Previous);
 	~Node();
-
 public:
-
-	void InsertHeadNode(int value);
-	void DeleteHeadNode();
-	void InsertTailNode(int value);
-	void DeleteTailNode();
-	void InsertArbitraryNode(int position, int value);
-	void DeleteArbitraryNode(int position);
-
-	int NodeCount();
-	Node* NodeFind(int position);
-	bool NodeCheck();
-
-	int ReturnHeadValue();
-	int ReturnTailValue();
-
-	void ListSort();
-
-protected:
-
-	
-
-public:
-
-	Node* head;
-	Node* tail;
 	
 	int _value;
 
 	Node* next;
 	Node* previous;
-
-	int tailVal;
-	int headVal;
-	int nodeTot;
-
 };
